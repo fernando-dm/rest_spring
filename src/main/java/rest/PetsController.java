92dee7bdbc94050c223c9ad3e8499536fe2d45aa
Fernando Garcia <fernando.garcia@etermax.com>
2018-10-13 13:26:49 -0300

    Creo modelo, repositororio y Rest Controller

---
+++ src/main/java/rest/PetsController.java
@@ -0,0 +1,19 @@
+package rest;
+
+import models.Pets;
+import repositories.PetsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pets")
+public class PetsController {
+    @Autowired
+    private PetsRepository repository;
+}
